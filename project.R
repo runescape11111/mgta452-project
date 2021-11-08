61c34683c6b9536884eab8385bac005f28e4b797
w3hsu <w3hsu@ucsd.edu>
2021-11-08 23:07:51 +0000

    Summer & Winter Olympics & GDP data

--- project.R
+++ project.R
@@ -11,3 +11,8 @@ print("hello Oliver")
 print("ohhhhh")
 
 print("hi there")
+library(tidyverse)
+athletes <- read_csv("olympic_athletes.csv")
+hosts <- read_csv("olympic_hosts.csv")
+medals <- read_csv("olympic_medals.csv")
+results <- read_csv("olympic_results.csv")
