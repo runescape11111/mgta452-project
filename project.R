2a0a5bae4f5acab1268465b353527f4b4c2a017c
yis042 <yis042@ucsd.edu>
2021-11-19 06:15:18 +0000

    cleaned up gdp data with column names, pivoting for future joins, and column types

--- project.R
+++ project.R
@@ -1,13 +1,25 @@
-print("hi")
-
-print("hello Oliver")
-
-print("ohhhhh")
-
-print("hi there")
 library(tidyverse)
+
 athletes <- read_csv("olympic_athletes.csv")
 hosts <- read_csv("olympic_hosts.csv")
 medals <- read_csv("olympic_medals.csv")
 results <- read_csv("olympic_results.csv")
+
+
 gdp <- read_csv("GDP.csv")
+gdp[gdp == '..'] <- NA
+colnames(gdp) <- c('name','code','series','series_code','1960',
+                   '1962','1964','1966','1968','1970',
+                   '1972','1974','1976','1978','1980',
+                   '1982','1984','1986','1988','1990',
+                   '1992','1994','1996','1998','2000',
+                   '2002','2004','2006','2008','2010',
+                   '2012','2014','2016','2018','2020')
+gdp_long <- pivot_longer(gdp, cols= starts_with(c('1','2')), names_to = 'year', values_to = 'value')
+
+gdp_long$series <- as.factor(gdp_long$series)
+levels(gdp_long$series)
+
+gdp_long$year <- as.numeric(gdp_long$year)
+gdp_long$value <- as.numeric(gdp_long$value)
+gdp_long[complete.cases(gdp_long),]
